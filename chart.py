a2dcd7e2839f15de6a2384d9767375eda9614f65
loveshijian <loveshijian@gmail.com>
2024-06-09 19:40:32 -0700

    update

--- chart.py
+++ chart.py
@@ -7,7 +7,9 @@ root = tkinter.Tk()
 root.withdraw()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
 
-df = pd.read_csv("ohlcv.csv")
+file_name = 'CME_1min_sample.csv'
+
+df = pd.read_csv(file_name)
 df['volume'] = 0
 df_future = df.tail(50)
 df_past = df.iloc[:-50]
@@ -27,16 +29,16 @@ def on_replay(target_chart: Chart):
 
 if __name__ == '__main__':
    
-    chart = Chart(toolbox=True, width=1600, height=900, inner_width=0.7)
+    chart = Chart(toolbox=True, width=1920, height=1080, inner_width=0.7)
     chart.unplayed_quotes = df_future
     chart.topbar.textbox('symbol', 'NQ')
     chart.topbar.switcher('timeframe', ('5 mins', '15 mins', '1 hour'), default='5 mins', func=on_timeframe_selection)
     chart.topbar.button('replay', 'Play', func=on_replay)
-    # chart = Chart(toolbox=True, width=1000, inner_width=0.6, inner_height=1)
-    # chart.legend(True)
+    chart.grid(vert_enabled=False, horz_enabled=False)
 
-    chart2 = chart.create_subchart(toolbox=True, position='right', width=0.3, height=1)
+    chart2 = chart.create_subchart(toolbox=False, position='right', width=0.3, height=1, sync=True)
     chart2.unplayed_quotes = df_future
+    chart2.grid(vert_enabled=False, horz_enabled=False)
 
     charts = [chart, chart2]
 
@@ -44,14 +46,14 @@ if __name__ == '__main__':
     chart2.set(df_past)
 
     # Imports the drawings saved in the JSON file.
-    chart.toolbox.import_drawings('drawings.json')
+    # chart.toolbox.import_drawings('drawings.json')
     
     # Loads the drawings under the default symbol.
-    chart.toolbox.load_drawings(chart.topbar['symbol'].value)  
+    # chart.toolbox.load_drawings(chart.topbar['symbol'].value)  
     
     # Saves drawings based on the symbol.
-    chart.toolbox.save_drawings_under(chart.topbar['symbol'])  
+    # chart.toolbox.save_drawings_under(chart.topbar['symbol'])  
     
     chart.show(block=True)
     # Exports the drawings to the JSON file upon close.
-    chart.toolbox.export_drawings('drawings.json')  
+    # chart.toolbox.export_drawings('drawings.json')  
